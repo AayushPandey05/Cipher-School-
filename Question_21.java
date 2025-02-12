d8729c639a24f97138c45325cf391b5d7caaa4a5
Aayush Pandey <139016137+AayushPandey05@users.noreply.github.com>
2025-02-12 13:10:41 +0530

    Question_21

---
+++ Question_21.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Question_21 {
+
+    public static void RotateMatrix(int[][] matrix){
+        int n = matrix.length;
+
+        //..Transpose the matrix;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //..Reverse each row
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int[][] matrix = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                matrix[i][j] = scn.nextInt();
+            }
+        }
+
+        RotateMatrix(matrix);
+
+        System.out.println("Rotated Matrix");
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
